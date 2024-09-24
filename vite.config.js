5b213321c16eb4f35196026c889cacc8d812cfa2
AKER <recrentic@gmail.com>
2024-09-25 02:02:19 +0500

    Configured gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/My-Portfolio/", // Update this to match your GitHub repository name
+});
